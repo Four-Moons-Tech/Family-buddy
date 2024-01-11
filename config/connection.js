0f9687ef1f5d9c84819a9a9d2f448bbaf6dec423
Anna Moon <anna@fourmoonstech.com>
2024-01-11 14:05:47 -0600

    fixed error creating chore, added update chore function

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,10 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  console.log('Using JAWS')
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  console.log('Using LOCAL')
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -14,7 +16,8 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: 3306,
+      logging: false
     }
   );
 }
--- public/js/addchore.js
+++ public/js/addchore.js
@@ -1,15 +1,27 @@
 // const saveButtonChoreModal = document.getElementById('child-modal-id');
+// console.log(saveButtonChoreModal)
 // console.log()
-function saveModalChore (event){
-  event.preventDefault();
-  const name = document.getElementById('chore-description').value;
-  const rate = document.getElementById('pay-rate').value;
+
+function handleModal(btn) {
+  // look for <form> inside of button's grandparent
+  const form = btn.parentElement.parentElement.querySelector(':scope form')
+  saveModalChore(
+    form.name.value,
+    form.rate.value,
+    btn.dataset.childid
+  )
+}
+
+function saveModalChore (name, rate, child_id){
+  // event.preventDefault();
+  // const name = document.getElementById('chore-description').value;
+  // const rate = document.getElementById('pay-rate').value;
   
 
   //console.log('name', name, 'rate', rate)
-   fetch('/child/child_id/chore', {
+   fetch('/api/chore', {
       method: 'POST',
-      body: JSON.stringify({ name, rate }),
+      body: JSON.stringify({ name, rate, child_id }),
       headers: { 'Content-Type': 'application/json' }
    }).then ((response)=> { 
      console.log("Reality Check No. 3")
@@ -22,8 +34,21 @@ function saveModalChore (event){
    }
    )
 }
-// saveButtonChoreModal.addEventListener('click', function (){
-//     console.log("hello ")
-// } )
 
-document.body.addEventListener('click', saveModalChore)
+function updateChore(input, choreId) {
+  const status = input.checked ? true : false
+  const child_id = input.dataset.childid
+  fetch(`/api/chore/${choreId}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({status, child_id})
+  })
+  .then((response)=>{
+    if (response.ok) {
+      document.location.replace('/add_child')
+    }
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
